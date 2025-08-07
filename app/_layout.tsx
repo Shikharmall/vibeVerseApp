d1d6f996d30a187218a613e5e3f48fea22758630
Shikhar Mall <shmall.21.2020@gmail.com>
2025-08-07 13:59:05 +0530

    added something

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,17 +14,15 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="Home" options={{ headerShown: false }} />
         <Stack.Screen name="(profile)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="ProfileForm" options={{ headerShown: false }} />
-        <Stack.Screen name="ProfileView" options={{ headerShown: false }} />
-        <Stack.Screen name="ProfileCard" options={{ headerShown: false }} /> */}
         <Stack.Screen name="(chat)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
+
+    </>
   );
 }
